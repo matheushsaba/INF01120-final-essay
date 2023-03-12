805769c9de090628086714f9bb4a32fb7992d9cb
Giulia <114960909+giustfnsk@users.noreply.github.com>
2023-03-12 18:07:13 -0300

    create graphical interface draft

---
+++ GUI/src/main/java/com/mycompany/gui/GUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gui;
+
+/**
+ *
+ * @author giuli
+ */
+public class GUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
